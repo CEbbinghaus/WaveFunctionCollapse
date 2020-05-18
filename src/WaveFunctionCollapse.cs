6b63ee701224280c8effdcb16e5367955eb98d3a
Christopher-Robin <jecebbi@gmail.com>
2020-05-18 11:44:42 +1000

    Set up Base Project

---
+++ src/WaveFunctionCollapse.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace wfc{
+    public class WaveFunctionCollapse{
+        public static void Print(){
+            Console.WriteLine("Writing a Line");
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using wfc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WaveFunctionCollapse.Print();
+        Console.WriteLine("Hello World!");
+    }
+}
